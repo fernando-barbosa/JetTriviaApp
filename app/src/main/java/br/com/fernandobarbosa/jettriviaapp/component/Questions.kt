528d8bad1a5a9a922592afe8cbbe86bd7a93364c
fernandobarbosa <fernandobsjunior@gmail.com>
2022-01-04 00:38:56 -0300

    Create a progress indicator composable

--- app/src/main/java/br/com/fernandobarbosa/jettriviaapp/component/Questions.kt
+++ app/src/main/java/br/com/fernandobarbosa/jettriviaapp/component/Questions.kt
@@ -1,10 +1,13 @@
 package br.com.fernandobarbosa.jettriviaapp.component
 
 import android.util.Log
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import br.com.fernandobarbosa.jettriviaapp.viewmodel.QuestionsViewModel
 
 @Composable
@@ -16,7 +19,7 @@ fun Questions(viewModel: QuestionsViewModel) {
     }
 
     if(viewModel.data.value.loading == true) {
-        CircularProgressIndicator()
+        ProgressIndicator()
     } else {
         val question = try {
             questions?.get(questionIndex.value)
@@ -36,3 +39,4 @@ fun Questions(viewModel: QuestionsViewModel) {
     }
 
 }
+
